bbcb968c6984ddd2a4903bf6d60811f2d9cf305c
Arijit Char <115190087+Arijit-Char@users.noreply.github.com>
2024-11-02 20:28:42 +0530

    Added Wallet Adapter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import './App.scss'
 import TokenLaunchpad from './TokenLaunchpad'
-
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
 function App() {
 
   return (
     <div>
-    <TokenLaunchpad/>
+      <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/R9GU0hMRumdyZ1wQvpvDBXHBsGWzJM67"}>
+        <WalletProvider wallets={[]} autoConnect>
+            <WalletModalProvider>
+              <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: 20
+              }}>
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+              <TokenLaunchpad/>
+            </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </div>
   )
 }
