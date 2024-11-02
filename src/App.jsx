8fbcf3efc64e8901bcbacf4289e3bfa16b0ea7f5
Arijit Char <115190087+Arijit-Char@users.noreply.github.com>
2024-11-02 20:19:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+import TokenLaunchpad from './TokenLaunchpad'
+
+function App() {
+
+  return (
+    <div>
+    <TokenLaunchpad/>
+    </div>
+  )
+}
+
+export default App
